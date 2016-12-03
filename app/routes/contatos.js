1e05c834d5074566175a2f1c218f5c59a93816a5
Onezino Moreira <zinogabriel@gmail.com>
2016-12-03 14:20:48 -0300

    recupera modelo de contatos
    reescreve rota contatos para ES6
    utiliza mock com objeto estaticos em rota contatos
    exemplo de uso do {{each}}

--- app/routes/contatos.js
+++ app/routes/contatos.js
@@ -1,4 +1,17 @@
-import Ember from 'ember';
 
-export default Ember.Route.extend({
-});
+class RouteContatos extends Ember.Route {
+
+	model() {
+		return [
+		{'nome': 'joao', 'telefone':'79-991122-22', 'email': 'email@google.com'},
+		{'nome': 'joao', 'telefone':'79-991122-22', 'email': 'email@google.com'},
+		{'nome': 'joao', 'telefone':'79-991122-22', 'email': 'email@google.com'},
+		{'nome': 'joao', 'telefone':'79-991122-22', 'email': 'email@google.com'},
+		{'nome': 'joao', 'telefone':'79-991122-22', 'email': 'email@google.com'},
+		{'nome': 'joao', 'telefone':'79-991122-22', 'email': 'email@google.com'},
+
+		];
+	}
+}
+
+export default RouteContatos
