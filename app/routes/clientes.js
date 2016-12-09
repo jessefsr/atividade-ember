9df40b6c7da68fb33d45e08434d543bbe930fac2
Onezino Moreira <zinogabriel@gmail.com>
2016-12-09 18:58:09 -0300

    implementa a exclusao

--- app/routes/clientes.js
+++ app/routes/clientes.js
@@ -4,5 +4,19 @@ export default Ember.Route.extend({
 
   model() {
     return this.get("store").findAll("cliente");
+  },
+
+  actions: {
+
+    confirmar(cliente) {
+      cliente.save();
+    },
+
+    cancelar(cliente) {
+      cliente.rollbackAttributes();
+    },
+    excluir(cliente) {
+        this.get("store").deleteRecord(cliente);
+    }
   }
 });
